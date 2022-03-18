9c40036a2d838609883f55a3ed333f3b1ab46161
F-Scienza <cufa1905@gmail.com>
2022-03-17 23:30:46 -0300

    reparada la funcion cards, no reconoce las imagenes

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import './card.css'
+import defaultImage from '../../Images/logo-M-scaled.webp'
 function Card(product, handleAddToCart) {
+	const prod = product.product
+	const prodimg = prod.img ? prod.img : defaultImage
     return (
 			<div className="Card-Container">
 				<div className='img-container'>
 					<img
 						className="product-img"
-						src={product.image}
-						alt="imagen de ejemplo"
+						src= {prodimg} 
+						alt="mementica"
 					/>
 				</div>
-				<h3 className="title">{product.title}</h3>
-				<p className="price">${product.price}</p>
+				<h3 className="title">{prod.title}</h3>
+				<p className="price">${prod.price}</p>
 				<p className="description">
-					{product.description}
+					{prod.description}
 				</p>
 				<button className="chart-btn" onClick={handleAddToCart}>Añadir al carrito</button>
 			</div>
