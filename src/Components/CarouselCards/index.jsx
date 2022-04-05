954f2f1de52de671a8dfabeeeede363a1a473a83
F-Scienza <cufa1905@gmail.com>
2022-04-05 10:09:43 -0300

    mejorando estilos carousel buttons

--- src/Components/CarouselCards/index.jsx
+++ src/Components/CarouselCards/index.jsx
@@ -3,47 +3,47 @@ import { Card } from '../Card'
 import AppContext from '../../Context/AppContext'
 import './CarouselCards.css'
 
-function CarouselCards() {
-    const { state, addToCart } = useContext(AppContext);
-    const { products } = state;
-    const handleAddToCart = product => () => {
-			addToCart(product);
-			console.log(product);
-		};
+function CarouselCards({carouselTitle}) {
+	const { state, addToCart } = useContext(AppContext);
+	const { products } = state;
+	const handleAddToCart = product => () => {
+		addToCart(product);
+		console.log(product);
+	};
 
-    const carousel = useRef(null)
-    const leftClick = e => {
-			e.preventDefault();
-            carousel.current.scrollLeft -= carousel.current.offsetWidth;
-            console.log(carousel.current.offsetWidth)
-		};
-    const rightClick = e => {
-			e.preventDefault();
-            carousel.current.scrollLeft += carousel.current.offsetWidth;
-            console.log(carousel.current.offsetWidth);
-		};
-    return (
-			<div className="carousel-container">
-				<h1>Productos principales</h1>
-				<div className="cards-container" ref={carousel}>
-					{products.map(product => (
-						<Card
-							key={product.id}
-							product={product}
-							handleAddToCart={handleAddToCart}
-						/>
-					))}
-				</div>
-				<div className="carousel-buttons">
-					<button className="btn-carousel-cards" onClick={leftClick}>
-						◀
-					</button>
-					<button className="btn-carousel-cards" onClick={rightClick}>
-						▶
-					</button>
-				</div>
+	const carousel = useRef(null);
+	const leftClick = e => {
+		e.preventDefault();
+		carousel.current.scrollLeft -= carousel.current.offsetWidth;
+		console.log(carousel.current.offsetWidth);
+	};
+	const rightClick = e => {
+		e.preventDefault();
+		carousel.current.scrollLeft += carousel.current.offsetWidth;
+		console.log(carousel.current.offsetWidth);
+	};
+	return (
+		<div className="carousel-container">
+			<h1 className='carousel-title'>{carouselTitle}</h1>
+			<div className="cards-container" ref={carousel}>
+				{products.map(product => (
+					<Card
+						key={product.id}
+						product={product}
+						handleAddToCart={handleAddToCart}
+					/>
+				))}
 			</div>
-		);
+			<div className="carousel-buttons">
+				<button className="btn-carousel-cards" onClick={leftClick}>
+					◀
+				</button>
+				<button className="btn-carousel-cards" onClick={rightClick}>
+					▶
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export { CarouselCards };
--- src/Containers/Home.jsx
+++ src/Containers/Home.jsx
@@ -2,12 +2,13 @@ import { CarouselCards } from '../Components/CarouselCards';
 import { Slider } from '../Components/Slider';
 import { CategoryCards } from '../Components/CategoryCards';
 function Home() {
-    return (  
-        <>
-            <CarouselCards />
-            <CategoryCards />
-        </>
-    );
+	return (
+		<>
+			<CarouselCards carouselTitle={'Destacados'} />
+			<CarouselCards carouselTitle={'Mas vendidos'} />
+			<CategoryCards />
+		</>
+	);
 }
 
 export { Home };
