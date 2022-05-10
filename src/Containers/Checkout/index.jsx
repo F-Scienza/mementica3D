cbad9383c507539a499c59a6941befdd40727b65
F-Scienza <cufa1905@gmail.com>
2022-05-10 18:58:51 -0300

    form checkout

--- src/Containers/Checkout/index.jsx
+++ src/Containers/Checkout/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import { AdressForm } from '../../Components/AdressForm/Index';
 import './Checkout.css'
 function Checkout() {
     return (
-			<div className="checkout-container">
-				<h1>hola soy el checkout ingrsa datos y comprame puto</h1>
-			</div>
+			<AdressForm/>
 		);
 }
 export { Checkout };
