39114dd7105ae19eb477c3c061e37b38eb374af4
F-Scienza <cufa1905@gmail.com>
2022-07-10 20:53:45 -0300

    handlesubmit paymethod

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -1,34 +1,50 @@
-import {useState} from 'react';
-
+import { useState } from 'react';
 
 export const useForm = (initalForm, validateForm) => {
-    const [form, setForm] = useState(initalForm)
-    const [errors, setErrors] = useState({})
-    const [loading, setLoading] = useState(false)
-    const [response, setResponse] = useState(null)
+	const [form, setForm] = useState(initalForm);
+	const [errors, setErrors] = useState({});
+	const [loading, setLoading] = useState(false);
+	const [response, setResponse] = useState(null);
+    const [mercadoPago, setMercadoPago] = useState(false)
+	const handleChange = e => {
+		const { name, value } = e.target;
+		setForm({
+			...form,
+			[name]: value,
+		});
+	};
 
-    const handleChange = e => {
-        const {name, value} = e.target
-        setForm({
-            ...form,
-            [name]: value
-        })
-    }
+	const handleBlur = e => {
+		handleChange(e);
+		setErrors(validateForm(form));
+	};
 
-    const handleBlur = e => {
-        handleChange(e)
-        setErrors(validateForm(form))
-    }
+	const handleSetMp = e => {
+		const { name, value } = e.target;
 
-    const handleSubmit = e => {}
+        value === 'mp' ? setMercadoPago(true) : setMercadoPago(false)
+	};
 
-    return {
-        form,
-        errors,
-        loading,
-        response,
-        handleBlur,
-        handleChange,
-        handleSubmit
-    }
-}
+	const handleSubmit = e => {
+		e.preventDefault();
+		setErrors(validateForm(form));
+
+		if (Object.keys(errors).length === 0) {
+			alert('enviando formulario');
+		} else {
+			return;
+		}
+	};
+
+	return {
+		form,
+		errors,
+		loading,
+		response,
+        mercadoPago,
+		handleBlur,
+		handleChange,
+		handleSubmit,
+		handleSetMp
+	};
+};
