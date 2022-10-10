95ed13e805b96245efaaaeabe934e2c42718fe8d
F-Scienza <cufa1905@gmail.com>
2022-10-09 22:49:03 -0300

    imagenessssss en fire base

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import './card.css'
-import defaultImage from '../../Images/logo-M-scaled.webp'
+//import defaultImage from '../../Images/logo-M-scaled.webp'
 const Card = ({ product, handleAddToCart, handleSetPmodal }) => {
+
 	return (
 		<div className="Card-Container">
 			<div className="img-container">
-				<img className="product-img" src={defaultImage} alt="product" />
+				<img className="product-img" src={product.image} alt='productimage' />
 			</div>
 			<h3 className="card-title">{product.title}</h3>
 			<p className="price">${product.price}</p>
 			<p className="description">{product.description}</p>
-			<button className="seemore" onClick={handleSetPmodal}>
-				{' '}
-				🔍{' '}
-			</button>
 			<button className="chart-btn" onClick={handleAddToCart(product)}>
 				Añadir al carrito
 			</button>
--- src/Components/CartCard/index.jsx
+++ src/Components/CartCard/index.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './CartCard.css'
-import defaultImage from '../../Images/logo-M-scaled.webp';
 import deleteIcon from '../../Images/Asets/delete.png'
 const CartCard = ({ product, handleRemoveFromCart }) => {
 	return (
 		<>
 			<div className="cardcart-container">
 				<h3 className='cartcard-title'>{product.title}</h3>
-				<img className="cartcard-img" src={defaultImage} alt="molde" />
+				<img className="cartcard-img" src={product.image} alt="molde" />
 				<h3 className="cartcard-price">${product.price}</h3>
 				<button
 					className="cartcard-btn"
